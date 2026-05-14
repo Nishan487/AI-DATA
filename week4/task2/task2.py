f813fb867e9acb8fbf9801d43801f179d88fc7ea
Nishan487 <nishanbaskota2064@gmail.com>
2026-05-14 23:41:01 +0545

    update task1,2,3,4 and add task 5

--- week4/task2/task2.py
+++ week4/task2/task2.py
@@ -1,24 +1,71 @@
-import pandas as pd,requests
+import pandas as pd
+import requests
+import mysql.connector
 
 try:
-    url="https://jsonplaceholder.typicode.com/posts"
-    data=requests.get(url).json()
-    df=pd.DataFrame(data)
-    
-    df=df[["userId","id","title","body"]]
+
+    url = "https://jsonplaceholder.typicode.com/posts"
+    data = requests.get(url).json()
+    df = pd.DataFrame(data)
+    df = df[["userId", "id", "title", "body"]]
     print(df.head(100))
-    
-    wordcount=df["title"].str.split().str.len()
-    df["word_count"]=wordcount
-    df=df[df["word_count"]>=4]
-    
-    df["title"]=df["title"].str.strip().str.title()
-    df["body"]=df["body"].str.strip()
-    df.to_csv("cleaned_posts.csv",index=False)
-    
+    print(df.info())
+    print(df.isnull().sum())
+    wordcount = df["title"].str.split().str.len()
+    df["word_count"] = wordcount
+    df = df[df["word_count"] >= 4]
+    df["title"] = (
+        df["title"]
+        .str.strip()
+        .str.title()
+    )
+
+    df["body"] = (
+        df["body"]
+        .str.strip()
+    )
+
+    df = df.dropna()
+    df.to_csv("cleaned_posts.csv", index=False)
     print(f"Total posts before cleaning: {len(data)}")
     print(f"Total posts after cleaning: {len(df)}")
-    
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Nishan@20640904",
+        database="etl_project"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cleaned_posts (
+        userId INT,
+        id INT,
+        title TEXT,
+        body TEXT,
+        word_count INT
+    )
+    """)
+    for _, row in df.iterrows():
+
+        cursor.execute("""
+        INSERT INTO cleaned_posts
+        (userId, id, title, body, word_count)
+        VALUES (%s, %s, %s, %s, %s)
+        """, (
+            row["userId"],
+            row["id"],
+            row["title"],
+            row["body"],
+            row["word_count"]
+        ))
+    conn.commit()
+
+    print("Data inserted successfully into MySQL")
+
     print(df.head())
+
 except Exception as e:
+
     print("Error fetching data:", e)
--- week4/task3/task03.py
+++ week4/task3/task03.py
@@ -1,47 +1,120 @@
-import pandas as pd 
+import pandas as pd
 import requests
-import sqlite3
+import mysql.connector
 
 def analyse_userAnd_post():
     try:
         url = "https://jsonplaceholder.typicode.com/users"
         data = requests.get(url).json()
         df_user = pd.json_normalize(data)
-        df_user = df_user[["id", "name","email","address.city"]]
-        df_user = df_user.rename(columns={"address.city": "city"})
-        # print(df_user)
-        
+        df_user = df_user[
+            ["id", "name", "email", "address.city"]
+        ]
+        df_user = df_user.rename(
+            columns={"address.city": "city"}
+        )
         url_post = "https://jsonplaceholder.typicode.com/posts"
         data = requests.get(url_post).json()
         df_post = pd.DataFrame(data)
         df_post = df_post[["userId", "title"]]
-        df_post= df_post.rename(columns={"userId": "id"})
-        # print(df_post)
-        
+        df_post = df_post.rename(
+            columns={"userId": "id"}
+        )
+
         merge_df = pd.merge(df_user, df_post, on="id")
-        # print(merge_df)
-        
-        post_count = df_post.groupby("id").size().reset_index(name="post_count")
-        df_user = pd.merge(df_user,post_count, on="id")
-        df_user["email"] = df_user["email"].str.lower()
 
-        df_user["name"] = df_user["name"].str.strip()
+        post_count = (
+            df_post.groupby("id")
+            .size()
+            .reset_index(name="post_count")
+        )
 
-        df_user["city"] = df_user["city"].str.strip()
-        print(df_user)
-        
+        df_user = pd.merge(
+            df_user,
+            post_count,
+            on="id"
+        )
+
+
+        df_user["email"] = (
+            df_user["email"]
+            .astype(str)
+            .str.lower()
+            .str.strip()
+        )
+
+        df_user["name"] = (
+            df_user["name"]
+            .astype(str)
+            .str.strip()
+        )
+
+        df_user["city"] = (
+            df_user["city"]
+            .astype(str)
+            .str.strip()
+        )
+
+
         df_user = df_user.dropna()
+
         merge_df = merge_df.dropna()
-        
-        print(df_user)
-        merge_df.to_csv("merged_data.csv", index=False)
-        conn=sqlite3.connect("merged_data.db")
-        merge_df.to_sql("merged_data", conn, if_exists="replace", index=False)
+
+
+        merge_df.to_csv(
+            "merged_data.csv",
+            index=False
+        )
+
+
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Nishan@20640904",
+            database="etl_project"
+        )
+
+        cursor = conn.cursor()
+
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS merged_data (
+            id INT,
+            name VARCHAR(255),
+            email VARCHAR(255),
+            city VARCHAR(255),
+            title TEXT
+        )
+        """)
+
+
+        for _, row in merge_df.iterrows():
+
+            cursor.execute("""
+            INSERT INTO merged_data
+            (id, name, email, city, title)
+            VALUES (%s, %s, %s, %s, %s)
+            """, (
+                row["id"],
+                row["name"],
+                row["email"],
+                row["city"],
+                row["title"]
+            ))
+
+
+        conn.commit()
+
+        print("Data inserted successfully")
+
+        print(merge_df.head())
+
+
     except Exception as e:
-        print("Error fetching data from API:", e)
-        return None
-        
+
+        print("Error:", e)
+
+
 if __name__ == "__main__":
+
     analyse_userAnd_post()
-        
-        
